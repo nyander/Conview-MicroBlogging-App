00935396136105963a723ec25ffa6b2ea8c8e1f6
Richard Nyande <rich.nyande@gmail.com>
2021-10-20 23:22:05 +0100

    Testing

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -20,7 +20,7 @@
 
                         <div>
                             <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded font-medium">
-                                Post
+                                Post Tests
                             </button>
                         </div>
                     </div>
